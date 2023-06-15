b6c9d9731a605bbfe18d15a7dc74b739f08dd206
Alireza Nilgaran <alirezanilgaran@gmail.com>
2023-06-15 23:04:53 +0330

    Create dummy version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='finops',
-    version='0.0.3',
+    version='0.0.4',
     description='Financial tools for the rest of us.',
     author='Alireza Nilgaran',
     author_email='alireza.nilgaran@gmail.com',

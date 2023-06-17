531a1d2689a63137357190f11d2b86affbe80b72
Alireza Nilgaran <alirezanilgaran@gmail.com>
2023-06-17 15:19:42 +0330

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='finops',
-    version='0.1.0',
+    version='0.1.1',
     description='Financial tools for the rest of us.',
     author='Alireza Nilgaran',
     author_email='alireza.nilgaran@gmail.com',

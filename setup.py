49af8f6379bbc81f6be0e5ce9cbcbe063fbab2e5
Alireza Nilgaran <alirezanilgaran@gmail.com>
2023-06-17 15:33:39 +0330

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='finops',
-    version='0.1.1',
+    version='0.1.2',
     description='Financial tools for the rest of us.',
     author='Alireza Nilgaran',
     author_email='alireza.nilgaran@gmail.com',
     url='https://github.com/nixuri/FinOps',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='BSD (3-clause)',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Analysts',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
     ],
     python_requires='>=3.6',
     install_requires=[
         'numpy',
         'pandas',
+        'requests',
+        'beautifulsoup4',
+        'datetime',
     ],
 )
